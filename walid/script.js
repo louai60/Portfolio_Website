7915b9096837c1eeb425208f54df49032ef6bc0f
louai60 <louaysaafi60@gmail.com>
2024-02-19 13:40:16 +0100

    portfolio

---
+++ walid/script.js
@@ -0,0 +1,13 @@
+
+// Zoom in on the image
+
+function zoomImage(element){
+  var im=document.querySelector("image1");
+  
+  if(im.style.width=="100%")
+  {
+    console.log("Code execute !");
+    im.style.width="200%";
+  }
+ 
+}
